5e3924479978f80058e854774c985028d30f7629
Jario Junior <56648231+rochajario@users.noreply.github.com>
2021-09-20 14:23:51 -0300

    Ajustes no container de DI

--- Gestao-Composicoes-Autorais-Api/Gestao-Composicoes-Autorais-Src/Startup.cs
+++ Gestao-Composicoes-Autorais-Api/Gestao-Composicoes-Autorais-Src/Startup.cs
@@ -31,21 +31,17 @@ namespace Gestao_Composicoes_Autorais_Src
             services.AddControllers(options => options.Filters.Add(typeof(HttpGlobalExceptionFilter)));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gestão Composições Autorais - API - v0.1.0", Version = "v0.1.0" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GestÃ£o ComposiÃ§Ãµes Autorais - API - v0.1.0", Version = "v0.1.0" });
             });
 
-            //TODO - Criar Container de Injeção de Dependência para Interfaces de Repositórios e Serviços de Domínio da Aplicação.
             services.AddDbContext<ApplicationContext>();
             services.AddTransient<IMusicasRepository, MusicasRepositoryExtendido>();
             services.AddTransient<IAutoresRepository, AutoresRepository>();
-
             services.AddTransient<IExceptionStrategyContextHandler, ExceptionStrategyContextHandler>();
             services.AddTransient(typeof(AutorConverter));
             services.AddTransient(typeof(MusicaConverter));
-
             services.AddTransient<IAutorService, AutorControllerServiceExtendido>();
             services.AddTransient<IMusicaService, MusicaControllerServiceExtendido>();
-
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -54,7 +50,7 @@ namespace Gestao_Composicoes_Autorais_Src
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestao Composições Autorais - API - v0.1.0"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestao ComposiÃ§Ãµes Autorais - API - v0.1.0"));
             }
 
             app.UseHttpsRedirection();
